700f5b4c11274638f39a27e2e875d982f42623d9
naginegi <2000vsteven@gmail.com>
2023-12-06 21:35:27 +0800

    myArticle_edit&preview

---
+++ src/views/negi/edit_myArticle.vue
@@ -0,0 +1,780 @@
+<template>
+    <div class="div">
+        <div class="div-2"></div>
+        <div class="div-3">
+            <div class="div-4">
+                <div class="column">
+                    <div class="div-5">
+                        <div class="div-6">
+                            論壇首頁<br />科普版<br />閒聊版<br />MY ARTICLE
+                        </div>
+                        <div class="div-7">
+                            <img loading="lazy"
+                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/63b36bbca1c643856fbf2055020cbb70138b7bbab25f53fb6c0712e573ba7646?"
+                                class="img" />
+                            <div class="div-8">我要發文</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column-2">
+                    <div class="div-9">
+                        <div class="div-10">
+                            <img loading="lazy"
+                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/b418320d3f737d9b5768a8ff7a9093328663500320d92e134ffd9ad410b7b27f?"
+                                class="img-2" /><img loading="lazy"
+                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/fbd1c2d61bd33be9b042fd0cdcff0230e8b0b58f79e7555ce44ee435db6ab7fa?"
+                                class="img-3" />
+                        </div>
+                        <div class="div-11">
+                            <div class="div-12">
+                                <div class="div-13">
+                                    <div class="column-3">
+                                        <div class="div-14">
+                                            <img loading="lazy"
+                                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/722d97a1bfb885c4d4e0d5e4dfe458b41e62e5581ba19e6b218b962d13f480f0?"
+                                                class="img-4" />
+                                            <div class="div-15">短腿貓的爸<br />@wei0113__</div>
+                                        </div>
+                                    </div>
+                                    <div class="column-4">
+                                        <div class="div-16">
+                                            <div class="div-17">with</div>
+                                            <div class="div-18">
+                                                <div class="div-19">Kiwi</div>
+                                                <img loading="lazy"
+                                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/b98ee66292a8d8df2af998353457daae6d0fa325c66e5662214968d867b39a8f?"
+                                                    class="img-5" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="div-20"></div>
+                            <div class="div-21">好像養了一隻迷因貓</div>
+                            <img loading="lazy" srcSet="..." class="img-6" />
+                            <div class="div-22">
+                                先上最愛的一張照片，<br />快樂迷因仔，<br />不知道是不是腿短又肥肥的，<br />每次覺得睡覺阿腿都很可愛<br />就是一隻小笨貓<br />哈哈
+                            </div>
+                            <div class="div-23">Comment</div>
+                            <div class="div-24"></div>
+                            <div class="div-25">
+                                <img loading="lazy"
+                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/90a7b1e78b5efecaa340c91a9bcbd4a44401c738482d2d93e101f95b3ef986fc?"
+                                    class="img-7" />
+                                <div class="div-26">
+                                    <div class="div-27">短腿貓的爸<br />@wei0113__</div>
+                                    <div class="div-28">笑死，這隻貓也太可愛</div>
+                                </div>
+                            </div>
+                            <div class="div-29">
+                                <img loading="lazy"
+                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/b7f136a6baad3854675063893a29010d38fc0656ee3890370fb6ebb844a51ef9?"
+                                    class="img-8" /><img loading="lazy"
+                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/102d5dc74ef3cdd9e136eb304a797de378f552775f4698b70e57d995853dc0db?"
+                                    class="img-9" /><img loading="lazy"
+                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/01c70fda5ed54f1a2b6706267352d538987115be56d97cd430547e61ec1ec33b?"
+                                    class="img-10" />
+                                <div class="div-30">
+                                    <div class="div-31">短腿貓的爸<br />@wei0113__</div>
+                                    <div class="div-32">哈哈謝拉&gt;&lt;</div>
+                                </div>
+                            </div>
+                            <div class="div-33"></div>
+                            <div class="div-34">
+                                <img loading="lazy"
+                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/cf0d8c3832c67190c7f1bb67eff4f5a433ae2ee5047da972caad5ac2ef6ba853?"
+                                    class="img-11" />
+                                <div class="div-35">
+                                    <div class="div-36">短腿貓的爸<br />@wei0113__</div>
+                                    <div class="div-37">笑死，這隻貓也太可愛</div>
+                                </div>
+                            </div>
+                            <div class="div-38"></div>
+                            <div class="div-39">
+                                <img loading="lazy"
+                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/cad0685797d6fcf53a2d8830f41bba36595e07631fd7b99b3ce44a96bcf380f2?"
+                                    class="img-12" />
+                                <div class="div-40">
+                                    <div class="div-41">短腿貓的爸<br />@wei0113__</div>
+                                    <div class="div-42">笑死，這隻貓也太可愛</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.div {
+    background-color: #dddfee;
+    display: flex;
+    padding-top: 6px;
+    flex-direction: column;
+}
+
+.div-2 {
+    box-shadow: 0px 4px 49px 0px rgba(0, 0, 0, 0.28);
+    background-color: #fff;
+    display: flex;
+    min-height: 139px;
+    width: 100%;
+    flex-direction: column;
+}
+
+@media (max-width: 991px) {
+    .div-2 {
+        max-width: 100%;
+    }
+}
+
+.div-3 {
+    align-self: center;
+    margin-top: 46px;
+    width: 100%;
+    max-width: 1290px;
+}
+
+@media (max-width: 991px) {
+    .div-3 {
+        max-width: 100%;
+        margin-top: 40px;
+    }
+}
+
+.div-4 {
+    gap: 20px;
+    display: flex;
+}
+
+@media (max-width: 991px) {
+    .div-4 {
+        flex-direction: column;
+        align-items: stretch;
+        gap: 0px;
+    }
+}
+
+.column {
+    display: flex;
+    flex-direction: column;
+    line-height: normal;
+    width: 20%;
+    margin-left: 0px;
+}
+
+@media (max-width: 991px) {
+    .column {
+        width: 100%;
+    }
+}
+
+.div-5 {
+    border-radius: 37px;
+    box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.25);
+    background-color: #fff;
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+    margin: 0 auto;
+    padding: 50px 21px 27px;
+}
+
+@media (max-width: 991px) {
+    .div-5 {
+        margin-top: 40px;
+        padding-right: 20px;
+    }
+}
+
+.div-6 {
+    color: #978989;
+    font: 400 28px Lexend, sans-serif;
+}
+
+.div-7 {
+    border-radius: 35px;
+    box-shadow: 0px 0px 8px 1px rgba(0, 0, 0, 0.28);
+    background-color: #6e75a8;
+    align-self: stretch;
+    display: flex;
+    margin-top: 111px;
+    justify-content: space-between;
+    gap: 11px;
+    padding: 17px 20px;
+}
+
+@media (max-width: 991px) {
+    .div-7 {
+        padding-left: 20px;
+        margin-top: 40px;
+    }
+}
+
+.img {
+    aspect-ratio: 1.03;
+    object-fit: contain;
+    object-position: center;
+    width: 32px;
+    overflow: hidden;
+    max-width: 100%;
+}
+
+.div-8 {
+    color: #fff;
+    text-align: right;
+    font: 800 31px Lexend, sans-serif;
+}
+
+.column-2 {
+    display: flex;
+    flex-direction: column;
+    line-height: normal;
+    width: 80%;
+    margin-left: 20px;
+}
+
+@media (max-width: 991px) {
+    .column-2 {
+        width: 100%;
+    }
+}
+
+.div-9 {
+    border-radius: 35px;
+    box-shadow: 0px 0px 8px 1px rgba(0, 0, 0, 0.28);
+    background-color: #fff;
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column;
+    align-items: end;
+    width: 100%;
+    padding: 50px 0;
+}
+
+@media (max-width: 991px) {
+    .div-9 {
+        max-width: 100%;
+        margin-top: 40px;
+    }
+}
+
+.div-10 {
+    display: flex;
+    width: 130px;
+    max-width: 100%;
+    padding-right: 5px;
+    align-items: start;
+    justify-content: space-between;
+    gap: 20px;
+    margin: 5px 72px 0 0;
+}
+
+@media (max-width: 991px) {
+    .div-10 {
+        margin-right: 10px;
+    }
+}
+
+.img-2 {
+    aspect-ratio: 1.33;
+    object-fit: contain;
+    object-position: center;
+    width: 100%;
+    overflow: hidden;
+    margin-top: 5px;
+    flex: 1;
+}
+
+.img-3 {
+    aspect-ratio: 1;
+    object-fit: contain;
+    object-position: center;
+    width: 100%;
+    overflow: hidden;
+    flex: 1;
+}
+
+.div-11 {
+    align-self: stretch;
+    display: flex;
+    flex-direction: column;
+    margin: 40px 0 4px;
+    padding: 0 17px;
+}
+
+@media (max-width: 991px) {
+    .div-11 {
+        max-width: 100%;
+    }
+}
+
+.div-12 {
+    align-self: stretch;
+}
+
+@media (max-width: 991px) {
+    .div-12 {
+        max-width: 100%;
+        padding-right: 20px;
+    }
+}
+
+.div-13 {
+    gap: 20px;
+    display: flex;
+}
+
+@media (max-width: 991px) {
+    .div-13 {
+        flex-direction: column;
+        align-items: stretch;
+        gap: 0px;
+    }
+}
+
+.column-3 {
+    display: flex;
+    flex-direction: column;
+    line-height: normal;
+    width: 57%;
+    margin-left: 0px;
+}
+
+@media (max-width: 991px) {
+    .column-3 {
+        width: 100%;
+    }
+}
+
+.div-14 {
+    display: flex;
+    margin-top: 16px;
+    gap: 17px;
+}
+
+@media (max-width: 991px) {
+    .div-14 {
+        margin-top: 40px;
+    }
+}
+
+.img-4 {
+    aspect-ratio: 1.03;
+    object-fit: contain;
+    object-position: center;
+    width: 96px;
+    overflow: hidden;
+    max-width: 100%;
+}
+
+.div-15 {
+    color: #978989;
+    margin: auto 0;
+    font: 800 32px Lexend, sans-serif;
+}
+
+.column-4 {
+    display: flex;
+    flex-direction: column;
+    line-height: normal;
+    width: 43%;
+    margin-left: 20px;
+}
+
+@media (max-width: 991px) {
+    .column-4 {
+        width: 100%;
+    }
+}
+
+.div-16 {
+    display: flex;
+    width: 100%;
+    flex-grow: 1;
+    align-items: end;
+    justify-content: space-between;
+    gap: 20px;
+}
+
+@media (max-width: 991px) {
+    .div-16 {
+        margin-top: 40px;
+    }
+}
+
+.div-17 {
+    color: #978989;
+    margin-top: 69px;
+    flex-grow: 1;
+    white-space: nowrap;
+    font: 800 32px Lexend, sans-serif;
+}
+
+@media (max-width: 991px) {
+    .div-17 {
+        margin-top: 40px;
+        white-space: initial;
+    }
+}
+
+.div-18 {
+    border-radius: 37px;
+    background-color: #e9d2a6;
+    align-self: stretch;
+    display: flex;
+    align-items: start;
+    justify-content: space-between;
+    gap: 4px;
+    padding: 14px 13px 0 23px;
+}
+
+@media (max-width: 991px) {
+    .div-18 {
+        padding-left: 20px;
+    }
+}
+
+.div-19 {
+    color: #fff;
+    flex-grow: 1;
+    white-space: nowrap;
+    font: 800 28px Lexend, sans-serif;
+}
+
+@media (max-width: 991px) {
+    .div-19 {
+        white-space: initial;
+    }
+}
+
+.img-5 {
+    aspect-ratio: 1.27;
+    object-fit: contain;
+    object-position: center;
+    width: 147px;
+    fill: #fff;
+    overflow: hidden;
+    margin-top: 10px;
+    max-width: 100%;
+}
+
+.div-20 {
+    background-color: rgba(151, 137, 137, 0.4);
+    align-self: stretch;
+    margin-top: 42px;
+    height: 5px;
+}
+
+@media (max-width: 991px) {
+    .div-20 {
+        max-width: 100%;
+        margin-top: 40px;
+    }
+}
+
+.div-21 {
+    color: #978989;
+    align-self: start;
+    white-space: nowrap;
+    margin: 27px 0 0 76px;
+    font: 800 39px Lexend, sans-serif;
+}
+
+@media (max-width: 991px) {
+    .div-21 {
+        margin-left: 10px;
+        white-space: initial;
+    }
+}
+
+.img-6 {
+    aspect-ratio: 0.9;
+    object-fit: contain;
+    object-position: center;
+    width: 367px;
+    box-shadow: 0px 0px 8px 1px rgba(0, 0, 0, 0.28);
+    overflow: hidden;
+    align-self: start;
+    max-width: 100%;
+    margin: 24px 0 0 67px;
+}
+
+@media (max-width: 991px) {
+    .img-6 {
+        margin-left: 10px;
+    }
+}
+
+.div-22 {
+    color: #978989;
+    align-self: start;
+    width: 325px;
+    max-width: 100%;
+    margin: 36px 0 0 76px;
+    font: 800 21px Lexend, sans-serif;
+}
+
+@media (max-width: 991px) {
+    .div-22 {
+        margin-left: 10px;
+    }
+}
+
+.div-23 {
+    color: #978989;
+    align-self: start;
+    white-space: nowrap;
+    margin: 151px 0 0 24px;
+    font: 800 28px Lexend, sans-serif;
+}
+
+@media (max-width: 991px) {
+    .div-23 {
+        white-space: initial;
+        margin: 40px 0 0 10px;
+    }
+}
+
+.div-24 {
+    background-color: rgba(151, 137, 137, 0.4);
+    align-self: stretch;
+    margin-top: 11px;
+    height: 5px;
+}
+
+@media (max-width: 991px) {
+    .div-24 {
+        max-width: 100%;
+    }
+}
+
+.div-25 {
+    align-self: start;
+    display: flex;
+    align-items: start;
+    gap: 17px;
+    margin: 34px 0 0 24px;
+}
+
+@media (max-width: 991px) {
+    .div-25 {
+        margin-left: 10px;
+    }
+}
+
+.img-7 {
+    aspect-ratio: 1.04;
+    object-fit: contain;
+    object-position: center;
+    width: 73px;
+    overflow: hidden;
+    max-width: 100%;
+}
+
+.div-26 {
+    display: flex;
+    flex-grow: 1;
+    flex-basis: 0%;
+    flex-direction: column;
+}
+
+.div-27 {
+    color: #978989;
+    font: 800 12px Lexend, sans-serif;
+}
+
+.div-28 {
+    color: #978989;
+    margin-top: 12px;
+    font: 800 21px Lexend, sans-serif;
+}
+
+.div-29 {
+    align-self: start;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    gap: 0px;
+    margin: 8px 0 0 58px;
+}
+
+@media (max-width: 991px) {
+    .div-29 {
+        margin-left: 10px;
+        justify-content: center;
+    }
+}
+
+.img-8 {
+    aspect-ratio: 0.03;
+    object-fit: contain;
+    object-position: center;
+    width: 2px;
+    stroke-width: 2px;
+    stroke: rgba(151, 137, 137, 0.5);
+    overflow: hidden;
+    align-self: start;
+    max-width: 100%;
+}
+
+.img-9 {
+    aspect-ratio: 5.33;
+    object-fit: contain;
+    object-position: center;
+    width: 80px;
+    stroke-width: 2px;
+    stroke: rgba(151, 137, 137, 0.5);
+    overflow: hidden;
+    align-self: start;
+    margin-top: 57px;
+    max-width: 100%;
+}
+
+@media (max-width: 991px) {
+    .img-9 {
+        margin-top: 40px;
+    }
+}
+
+.img-10 {
+    aspect-ratio: 1.03;
+    object-fit: contain;
+    object-position: center;
+    width: 73px;
+    overflow: hidden;
+    align-self: end;
+    margin-top: 26px;
+    max-width: 100%;
+}
+
+.div-30 {
+    align-self: center;
+    display: flex;
+    flex-grow: 1;
+    flex-basis: 0%;
+    flex-direction: column;
+    margin: auto 0;
+}
+
+.div-31 {
+    color: #978989;
+    font: 800 12px Lexend, sans-serif;
+}
+
+.div-32 {
+    color: #978989;
+    margin-top: 12px;
+    font: 800 21px Lexend, sans-serif;
+}
+
+.div-33 {
+    background-color: rgba(151, 137, 137, 0.4);
+    align-self: stretch;
+    margin-top: 16px;
+    height: 2px;
+}
+
+@media (max-width: 991px) {
+    .div-33 {
+        max-width: 100%;
+    }
+}
+
+.div-34 {
+    align-self: start;
+    display: flex;
+    align-items: start;
+    gap: 17px;
+    margin: 24px 0 0 24px;
+}
+
+@media (max-width: 991px) {
+    .div-34 {
+        margin-left: 10px;
+    }
+}
+
+.img-11 {
+    aspect-ratio: 1.04;
+    object-fit: contain;
+    object-position: center;
+    width: 73px;
+    overflow: hidden;
+    max-width: 100%;
+}
+
+.div-35 {
+    display: flex;
+    flex-grow: 1;
+    flex-basis: 0%;
+    flex-direction: column;
+}
+
+.div-36 {
+    color: #978989;
+    font: 800 12px Lexend, sans-serif;
+}
+
+.div-37 {
+    color: #978989;
+    margin-top: 11px;
+    font: 800 21px Lexend, sans-serif;
+}
+
+.div-38 {
+    background-color: rgba(151, 137, 137, 0.4);
+    align-self: stretch;
+    margin-top: 33px;
+    height: 2px;
+}
+
+@media (max-width: 991px) {
+    .div-38 {
+        max-width: 100%;
+    }
+}
+
+.div-39 {
+    align-self: start;
+    display: flex;
+    align-items: start;
+    gap: 17px;
+    margin: 30px 0 0 24px;
+}
+
+@media (max-width: 991px) {
+    .div-39 {
+        margin-left: 10px;
+    }
+}
+
+.img-12 {
+    aspect-ratio: 1.04;
+    object-fit: contain;
+    object-position: center;
+    width: 73px;
+    overflow: hidden;
+    max-width: 100%;
+}
+
+.div-40 {
+    display: flex;
+    flex-grow: 1;
+    flex-basis: 0%;
+    flex-direction: column;
+}
+
+.div-41 {
+    color: #978989;
+    font: 800 12px Lexend, sans-serif;
+}
+
+.div-42 {
+    color: #978989;
+    margin-top: 12px;
+    font: 800 21px Lexend, sans-serif;
+}</style>
