5d0ff18253df05dd6d415551d6fdc7e326b1540b
Ritsu720 <julie890909@gmail.com>
2023-12-06 11:13:09 +0800

    bootstrap & pinia

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import './assets/main.scss'
+import 'bootstrap'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { createPinia } from 'pinia'
+
 const app = createApp(App)
+const pinia = createPinia(App)
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
---
+++ src/stores/indexState.js
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+
+export default defineStore("indexState", {
+    // data
+    state: () =>({
+
+    }),
+    // computed
+    getters: {
+
+    },
+    // methods
+    actions: {
+
+    }
+})
