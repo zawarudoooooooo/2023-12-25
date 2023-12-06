247474b4d908cd7dfe3d75bf1927882bc84b38c5
Ritsu720 <julie890909@gmail.com>
2023-12-06 13:16:48 +0800

    ProfileDashBoard change style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,22 @@
 <script>
+import { RouterLink } from 'vue-router';
+export default{
+    date(){
+        return{
+
+        }
+    },
+    components: {
+        RouterLink
+    }
+}
 </script>
 
 <template>
     <div class="header">
+        <RouterLink to="/Login">Log In</RouterLink>
+        <RouterLink to="/SignUp">Sign Up</RouterLink>
+        <RouterLink to="/Profile">Profile</RouterLink>
     </div>
 </template>
 
@@ -11,5 +25,6 @@
         width: 100vw;
         height: 15vh;
         background-color: white;
+        justify-content: space-between;
     }
 </style>
