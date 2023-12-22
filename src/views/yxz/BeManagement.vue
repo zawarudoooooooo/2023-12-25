b6ea69cdec3dcdf96602909698ae024fbcae4a13
Mebice <147121651+Mebice@users.noreply.github.com>
2023-12-22 17:39:09 +0800

    後台 消息更新刪除

--- src/views/yxz/BeManagement.vue
+++ src/views/yxz/BeManagement.vue
@@ -12,6 +12,7 @@ export default {
                 image: null,
             },
             newInfoList: null,
+
         };
     },
     mounted() {
@@ -86,6 +87,37 @@ export default {
                 });
         },
 
+        deleteInfo(serialNo) {
+            const apiUrl = 'http://localhost:8080/api/adoption/deleteNewUserInfo';
+
+            fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    serialNo: serialNo,
+                }),
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! Status: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    // 处理删除成功的逻辑
+                    console.log('Deleted successfully:', data);
+                    alert('删除成功');
+
+                    // 刷新信息列表（或执行其他需要的逻辑）
+                    this.searchAllNewInfo();
+                })
+                .catch(error => {
+                    // 处理删除失败的逻辑
+                    console.error('Error:', error);
+                });
+        },
 
         handleFileUpload(event) {
             const file = event.target.files[0];
@@ -100,7 +132,85 @@ export default {
                 reader.readAsDataURL(file);
             }
         },
-    }
+
+        //更新邏輯
+        handleImageUpload(newInfo) {
+            const fileInput = event.target;
+            const file = fileInput.files[0];
+
+            if (file) {
+                const reader = new FileReader();
+
+                reader.onloadend = () => {
+                    // 去除 base64 圖片數據的前綴部分
+                    const base64WithoutPrefix = reader.result.split(',')[1];
+
+                    // 將處理後的 base64 圖片數據存儲到 newInfo 中
+                    newInfo.image = base64WithoutPrefix;
+                    // 將處理後的 base64 圖片數據存儲到 newInfo 中
+                    newInfo.editedImage = `data:image/jpeg;base64,${base64WithoutPrefix}`;
+
+                };
+
+                // 讀取文件為 base64 格式
+                reader.readAsDataURL(file);
+            }
+        },
+
+
+
+        editInfo(serialNo) {
+            // 找到點擊的div
+            const clickedInfo = this.newInfoList.find(info => info.serialNo === serialNo);
+            // 將該div標記為編輯狀態
+            clickedInfo.editing = true;
+
+            // 初始化編輯欄的數據（從原始貼文中獲取）
+            clickedInfo.editedTitle = clickedInfo.title;
+            clickedInfo.editedCategory = clickedInfo.category;
+            clickedInfo.editedDate = clickedInfo.date;
+            clickedInfo.editedContent = clickedInfo.content;
+
+            // 如果需要，也可以在這裡初始化其他編輯欄的數據
+        },
+
+        saveInfo(serialNo) {
+            // 找到點擊的div
+            const clickedInfo = this.newInfoList.find(info => info.serialNo === serialNo);
+            console.log(clickedInfo)
+            // 呼叫後端保存 API
+            fetch('http://localhost:8080/api/adoption/updateUserInfo', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    title: clickedInfo.editedTitle,
+                    content: clickedInfo.editedContent,
+                    date: clickedInfo.editedDate,
+                    category: clickedInfo.editedCategory,
+                    image: clickedInfo.image,
+                    serialNo: serialNo,
+                }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+
+                    // 保存成功後，將該div標記為非編輯狀態
+                    clickedInfo.editing = false;
+                    // 刷新信息列表（或執行其他需要的邏輯）
+                    this.searchAllNewInfo();
+                    // 顯示成功信息
+                    Swal.fire('Update successful');
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    // 處理錯誤情況
+                });
+        },
+    },
+
 
 };
 </script>
@@ -131,15 +241,66 @@ export default {
         </table>
         <button @click="createNewInfo">Create New Info</button>
     </div>
+
+
     <div class="searchAllNewInfo">
+        <div v-for="newInfo in newInfoList" :key="newInfo.serialNo" class="info-card"
+            :class="{ 'editing': newInfo.editing }">
+            {{ newInfo.serialNo }}
+            <i class="fa-solid fa-trash" @click="deleteInfo(newInfo.serialNo)"></i>
+
+            <i v-if="!newInfo.editing" class="edit-icon" @click="editInfo(newInfo.serialNo)">&#9998;</i>
+            <i v-else class="save-icon" @click="saveInfo(newInfo.serialNo)">&#128190;</i>
+
+            <h2 v-if="!newInfo.editing">{{ newInfo.title }}</h2>
+            <textarea v-else v-model="newInfo.editedTitle" style="width: 100%; height: 100px;"></textarea>
+
+            <p v-if="!newInfo.editing">Category: {{ newInfo.category }}</p>
+            <input v-else type="text" v-model="newInfo.editedCategory" style="width: 100%;" />
+
+            <p v-if="!newInfo.editing">Date: {{ newInfo.date }}</p>
+            <input v-else type="date" v-model="newInfo.editedDate" style="width: 100%;" />
+
+            <p v-if="!newInfo.editing">Content: {{ newInfo.content }}</p>
+            <textarea v-else v-model="newInfo.editedContent" style="width: 100%; height: 200px;"></textarea>
+
+            <!-- 文件選擇器 -->
+            <input v-if="newInfo.editing" type="file" @change="handleImageUpload(newInfo)" />
+
+            <!-- 顯示圖片 -->
+            <img v-if="!newInfo.editedImage" :src="newInfo.base64Image" alt="New Info Image" />
+
+            <!-- 顯示編輯模式中的預覽圖片 -->
+            <img v-if="newInfo.editing " :src="newInfo.editedImage" alt="Edited Image" />
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- <div class="searchAllNewInfo">
         <div v-for="newInfo in newInfoList" :key="newInfo.serialNo" class="info-card">
+            {{ newInfo.serialNo }}
+            <i class="trash-icon" @click="deleteInfo(newInfo.serialNo)">&#128465;</i>
             <h2>{{ newInfo.title }}</h2>
             <p>Category: {{ newInfo.category }}</p>
             <p>Date: {{ newInfo.date }}</p>
             <p>Content: {{ newInfo.content }}</p>
             <img v-if="newInfo.base64Image" :src="newInfo.base64Image" alt="New Info Image" />
+
         </div>
-    </div>
+    </div> -->
 </template>
 
 <style lang="scss" scoped>
@@ -205,6 +366,7 @@ export default {
 }
 
 .info-card {
+    position: relative;
     border: 1px solid #ddd;
     border-radius: 8px;
     padding: 16px;
@@ -212,7 +374,33 @@ export default {
     /* 調整每個消息卡片的寬度，這裡假設每行三個 */
     box-sizing: border-box;
     background-color: #f9f9f9;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    position: relative;
 }
+
+.fa-trash {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    font-size: 1.5rem;
+    /* 這裡可以根據需要的大小調整 */
+    cursor: pointer;
+    z-index: 1;
+    /* 確保垃圾桶圖示在其他內容之上 */
+}
+
+.edit-icon {
+    position: absolute;
+    top: -10px;
+    right: 45px;
+    font-size: 2.5rem;
+    cursor: pointer;
+    z-index: 1;
+    /* 確保垃圾桶圖示在其他內容之上 */
+}
+
 
 .info-card h2 {
     font-size: 1.5rem;
